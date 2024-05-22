76af6fc3bb17d9467ab7407f6feba22b75f5a114
たいせい <119865966+Taisei-Yamaguchi@users.noreply.github.com>
2024-05-22 06:34:52 -0700

    init accounts app

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    
+    # startapp
+    "accounts.apps.AccountsConfig",
 ]
 
 MIDDLEWARE = [
@@ -129,3 +132,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = 'accounts.Account'
